5ed60becc71cd26d39b7beaafa12cbc0f3aed637
Daniel <mwaniel03@gmail.com>
2024-10-17 10:24:13 +0300

    Updatetask  and gettask function

--- lumen/app/Http/Controllers/TaskController.php
+++ lumen/app/Http/Controllers/TaskController.php
@@ -40,4 +40,42 @@ class TaskController extends Controller
     return response()->json($tasks->paginate(10));
 
    }
+
+   //get a task
+   public function getTask($id)
+   {
+    $task = Task::find($id);
+    if (!$task){
+        return response()->json(['message'=>'Task not found'],404);
+    }
+    return response()->json($task);
+   }
+   //update a task
+   public function updateTask(Request $request,$id)
+   {
+    $task = Task::find($id);
+    if (!$task){
+        return response()->json(['message'=>'Task not found'],404);
+    }
+    $validator = Validator::make($request->all(),[
+        'title'=>'required|unique:tasks',
+        'due_date'=> 'required|date|after:today'
+    ]);
+    if ($validator->fails()){
+        return response()->json($validator->errors(),400);
+    }
+    $task->update($request->all());
+    return response()->json(['message'=>'Task updated successfully'],200);
+   }
+
+   //delete
+   public function deleteTask($id)
+   {
+    $task = Task::find($id);
+    if (!$task){
+        return response()->json(['message'=>'Task not found'],404);
+    }
+    $task->delete();
+    return response()->json(['message'=>'Task deleted successfully'],200);
+   }
 }
